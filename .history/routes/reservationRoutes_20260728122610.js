529e37d1968584846b92855fbe15cd86d4a9772a
abdullah101206 <abdullah101206@gmail.com>
2026-07-30 11:30:43 +0500

    Remove history folder and secret backups

--- .history/routes/reservationRoutes_20260728122610.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createReservation } = require('../controllers/reservationController');
-
-// Sirf public table reservation route
-router.post('/', createReservation);
-
-module.exports = router;
